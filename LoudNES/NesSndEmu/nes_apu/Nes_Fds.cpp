5b2b4464b28dad3256a294f5295472fba747a68e
Matt Montag <matt.montag@gmail.com>
2021-02-16 00:33:50 -0800

    Add NesSndEmu sources
    Except for:
    
    Sound_Queue.cpp
    Sound_Queue.h
    Wave_Writer.cpp
    Wave_Writer.hpp
    demo.cpp

---
+++ LoudNES/NesSndEmu/nes_apu/Nes_Fds.cpp
@@ -0,0 +1,268 @@
+// Famicom Disk System audio chip emulator for FamiStudio.
+// Added to Nes_Snd_Emu by @NesBleuBleu, mostly adapted from Disch / NotSoFatso
+
+#include "Nes_Fds.h"
+#include <string.h>
+
+#include BLARGG_SOURCE_BEGIN
+
+Nes_Fds::Nes_Fds() : vol(1.0f)
+{
+	output(NULL);
+	volume(1.0);
+	reset();
+}
+
+Nes_Fds::~Nes_Fds()
+{
+}
+
+void Nes_Fds::reset()
+{
+	last_time = 0;
+	memset(&osc.wave, 0, sizeof(osc.wave));
+	memset(&osc.modt, 0, sizeof(osc.modt));
+	memset(&osc.regs, 0, sizeof(osc.regs));
+	osc.mod_pos = 0;
+	osc.mod_phase = 0;
+	osc.mod_pos = 0;
+	osc.delay = 0;
+	osc.last_amp = 0;
+	osc.phase = 0;
+	osc.volume_env = 0x20;
+	osc.regs[10] = 0xff;
+}
+
+void Nes_Fds::volume(double v)
+{
+	vol = v;
+	update_volume();
+}
+
+void Nes_Fds::update_volume()
+{
+	float masterVolume = 1.0f;
+
+	switch (osc.regs[9] & 0x03)
+	{
+		case 1: masterVolume = 2.0f / 3.0f; break;
+		case 2: masterVolume = 2.0f / 4.0f; break;
+		case 3: masterVolume = 2.0f / 5.0f; break;
+	}
+
+	synth.volume(vol * masterVolume * 0.13f);
+}
+
+void Nes_Fds::treble_eq(blip_eq_t const& eq)
+{
+	synth.treble_eq(eq);
+}
+
+void Nes_Fds::output(Blip_Buffer* buf)
+{
+	osc.output = buf;
+}
+
+void Nes_Fds::write_register(cpu_time_t time, cpu_addr_t addr, int data)
+{
+	require(addr >= wave_addr && addr < (regs_addr + regs_count));
+	require((unsigned)data <= 0xff);
+
+	run_until(time);
+
+	if (addr >= wave_addr && addr < (wave_addr + wave_count))
+	{
+		if (osc.regs[9] & 0x80)
+			osc.wave[addr - wave_addr] = (data & 0x3f) - 0x20;
+	}
+	else
+	{
+		cpu_addr_t reg = addr - regs_addr;
+		
+		switch (reg)
+		{
+		case 0:
+			// TODO: Volume envelope support.
+			require(data & 0x80);
+			osc.volume_env = data & 0x3f;
+			break;
+		case 4:
+			// TODO: Sweep envelope support.
+			require(data & 0x80);
+			break;
+		case 5:
+			osc.mod_pos = data & 0x7f;
+			break;
+		case 7:
+			if (data & 0x80) 
+				osc.mod_phase = osc.mod_phase & 0x3F0000; 
+			break;
+		case 8:
+			// TODO: Ring buffer? I cant imagine that's of the hardware does it.
+			memcpy(&osc.modt[0], &osc.modt[2], modt_count - 2); 
+			osc.modt[modt_count - 2] = data;
+			osc.modt[modt_count - 1] = data;
+			break;
+		}
+
+		osc.regs[reg] = data;
+
+		if (reg == 9)
+		{
+			update_volume();
+		}
+	}
+}
+
+void Nes_Fds::end_frame(cpu_time_t time)
+{
+	if (time > last_time)
+		run_until(time);
+	last_time -= time;
+	assert(last_time >= 0);
+}
+
+void Nes_Fds::run_until(cpu_time_t time)
+{
+	require(time >= last_time);
+	run_fds(time);
+	last_time = time;
+}
+
+#include BLARGG_ENABLE_OPTIMIZER
+
+void Nes_Fds::run_fds(cpu_time_t end_time)
+{
+	require(end_time >= last_time);
+
+	if (!osc.output)
+		return;
+
+	// Code here is kind of a mix of Disch/NotSoFatso + NSFPlay.
+	bool mod_on = osc.mod_period() && !(osc.regs[7] & 0x80);
+	bool wav_on = osc.wav_period() && !(osc.regs[3] & 0x80) && !(osc.regs[9] & 0x80);
+
+	cpu_time_t time = last_time;
+
+	time += osc.delay;
+	osc.delay = 0;
+	int last_amp = osc.last_amp;
+
+	while (time < end_time)
+	{
+		int sub_step = min(16, end_time - time);
+
+		// Modulation
+		if (mod_on)
+		{
+			const int modulation_table[8] = { 0,1,2,4,0,-4,-2,-1 };
+
+			int start_pos = osc.mod_phase >> 16;
+			osc.mod_phase += (sub_step * osc.mod_period());
+			int end_pos = osc.mod_phase >> 16;
+
+			osc.mod_phase = osc.mod_phase & 0x3fffff;
+
+			for (int p = start_pos; p < end_pos; ++p)
+			{
+				int wv = osc.modt[p & 0x3f];
+				osc.mod_pos = wv == 4 ? 0 : osc.mod_pos + modulation_table[wv];
+				osc.mod_pos &= 0x7f;
+			}
+		}
+
+		// Wave generation
+		if (wav_on)
+		{
+			int mod = 0;
+			int sweep_gain = osc.regs[4] & 0x3f; // TODO: Sweep envelopes.
+
+			if (sweep_gain)
+			{
+				int pos = (osc.mod_pos < 64) ? osc.mod_pos : (osc.mod_pos - 128);
+
+				while (pos >= 64) pos -= 128;
+				while (pos < -64) pos += 128;
+
+				int temp = pos * sweep_gain;
+				int rem = temp & 0xf;
+				temp >>= 4;
+				if ((rem > 0) && ((temp & 0x80) == 0))
+					temp += pos < 0 ? -1 : 2;
+
+				while (temp >= 192) temp -= 256;
+				while (temp <  -64) temp += 256;
+
+				temp *= osc.wav_period();
+				rem = temp & 0x3f;
+				temp >>= 6;
+				if (rem >= 32)
+					temp++;
+
+				mod = temp;
+			}
+
+			int f = osc.wav_period() + mod;
+			osc.phase = osc.phase + (sub_step * f);
+			osc.phase = osc.phase & 0x3fffff;
+		}
+		
+		int volume = min(osc.volume_env, 0x20);
+		int amp = osc.wave[(osc.phase >> 16) & 0x3f] * volume;
+
+		int delta = amp - last_amp;
+		if (delta)
+		{
+			synth.offset(time, delta, osc.output);
+			last_amp = amp;
+		}
+
+		time += sub_step;
+	}
+
+	osc.last_amp = last_amp;
+	osc.delay = time - end_time;
+}
+
+void Nes_Fds::start_seeking()
+{
+	shadow_modt_idx = 0;
+	memset(shadow_regs, -1, sizeof(shadow_regs));
+	memset(shadow_wave,  0, sizeof(shadow_wave));
+	memset(shadow_modt,  0, sizeof(shadow_modt));
+}
+
+void Nes_Fds::stop_seeking(blip_time_t& clock)
+{
+	memcpy(osc.modt, shadow_modt, modt_count);
+	memcpy(osc.wave, shadow_wave, wave_count);
+
+	for (int i = 0; i < array_count(shadow_regs); i++)
+	{
+		if (shadow_regs[i] >= 0)
+			write_register(clock += 4, regs_addr + i, shadow_regs[i]);
+	}
+}
+
+void Nes_Fds::write_shadow_register(int addr, int data)
+{
+	if (addr >= wave_addr && addr < (wave_addr + wave_count))
+	{
+		// Ignore write enable.
+		shadow_wave[addr - wave_addr] = data;
+	}
+	else if (addr >= regs_addr && addr < (regs_addr + regs_count))
+	{
+		if (addr == 0x4088)
+		{
+			// Assume we always write to mod table in batch of 32. This is true for FamiStudio.
+			shadow_modt[shadow_modt_idx + 0] = data;
+			shadow_modt[shadow_modt_idx + 1] = data;
+			shadow_modt_idx = (shadow_modt_idx + 2) % modt_count;
+		}
+		else
+		{
+			shadow_regs[addr - regs_addr] = data;
+		}
+	}
+}
---
+++ LoudNES/NesSndEmu/nes_apu/Nes_Namco.cpp
@@ -0,0 +1,168 @@
+
+// Nes_Snd_Emu 0.1.7. http://www.slack.net/~ant/libs/
+
+#include "Nes_Namco.h"
+
+/* Copyright (C) 2003-2005 Shay Green. This module is free software; you
+can redistribute it and/or modify it under the terms of the GNU Lesser
+General Public License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version. This
+module is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for
+more details. You should have received a copy of the GNU Lesser General
+Public License along with this module; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA */
+
+#include BLARGG_SOURCE_BEGIN
+
+Nes_Namco::Nes_Namco()
+{
+	output( NULL );
+	volume( 1.0 );
+	reset();
+}
+
+Nes_Namco::~Nes_Namco()
+{
+}
+
+void Nes_Namco::reset()
+{
+	addr_reg = 0;
+	active_osc = osc_count - 1;
+	delay = 0;
+	last_amp = 0;
+	last_time = 0;
+	
+	int i;
+	for ( i = 0; i < reg_count; i++ )
+		reg [i] = 0;
+	
+	for ( i = 0; i < osc_count; i++ )
+	{
+		Namco_Osc& osc = oscs [i];
+		osc.delay = 0;
+		osc.sample = 0;
+	}
+}
+
+void Nes_Namco::output( Blip_Buffer* buf )
+{
+	buffer = buf;
+	for ( int i = 0; i < osc_count; i++ )
+		osc_output( i, buf );
+}
+
+BOOST::uint8_t& Nes_Namco::access()
+{
+	int addr = addr_reg & 0x7f;
+	if ( addr_reg & 0x80 )
+		addr_reg = (addr + 1) | 0x80;
+	return reg [addr];
+}
+
+void Nes_Namco::end_frame( cpu_time_t time )
+{
+	if ( time > last_time )
+		run_until( time );
+	
+	last_time -= time;
+	assert( last_time >= 0 );
+}
+
+#include BLARGG_ENABLE_OPTIMIZER
+
+void Nes_Namco::run_until(cpu_time_t end_time)
+{
+	require(end_time >= last_time);
+
+	int active_oscs = ((reg[0x7f] >> 4) & 7) + 1;
+
+	cpu_time_t time = last_time + delay;
+
+	while (time < end_time)
+	{
+		Namco_Osc& osc = oscs[active_osc];
+
+		BOOST::uint8_t* osc_reg = &reg[active_osc * 8 + 0x40];
+
+		long freq = ((osc_reg[4] & 3) << 16) | (osc_reg[2] << 8) | osc_reg[0];
+		int volume = osc_reg[7] & 15;
+		int wave_size = (8 - ((osc_reg[4] >> 2) & 7)) * 4;
+
+		// This is not very accurate. We always do the entire 15-cycle channel update.
+		// We should only update until end_time. This will fail to emulate mid-update
+		// register changes, but in practice should be OK.
+		if (osc.output && volume && freq && wave_size)
+		{
+			// read wave sample
+			long phase = (osc_reg[5] << 16) | (osc_reg[3] << 8) | osc_reg[1];
+			phase = (phase + freq) % (wave_size << 16);
+
+			int addr = ((phase >> 16) + osc_reg[6]) & 0xff;
+			int sample = reg[addr >> 1];
+
+			if (addr & 1)
+				sample >>= 4;
+
+			osc.sample = (sample & 15) * volume;
+
+			osc_reg[5] = (phase >> 16) & 0xff;
+			osc_reg[3] = (phase >>  8) & 0xff;
+			osc_reg[1] = (phase >>  0) & 0xff;
+		}
+		else
+		{
+			osc.sample = 0;
+		}
+
+		float sum = 0.0f;
+		for (int i = osc_count - active_oscs; i < osc_count; i++)
+			sum += oscs[i].sample;
+		int sample = (int)(sum / min(6, max(1, active_oscs)) + 0.5f); 
+
+		// output impulse if amplitude changed
+		int delta = sample - last_amp;
+		if (delta)
+		{
+			last_amp = sample;
+			synth.offset(time, delta, buffer);
+		}
+
+		time += osc_update_time;
+
+		if (--active_osc < osc_count - active_oscs)
+			active_osc = osc_count - 1;
+	}
+
+	delay = time - end_time;
+	last_time = time;
+}
+
+void Nes_Namco::start_seeking()
+{
+	memset(shadow_internal_regs, -1, sizeof(shadow_internal_regs));
+}
+
+void Nes_Namco::stop_seeking(blip_time_t& clock)
+{
+	for (int i = 0; i < array_count(shadow_internal_regs); i++)
+	{
+		if (shadow_internal_regs[i] >= 0)
+		{
+			write_register(clock += 4, addr_reg_addr, i);
+			write_register(clock += 4, data_reg_addr, shadow_internal_regs[i]);
+		}
+	}
+}
+
+void Nes_Namco::write_shadow_register(int addr, int data)
+{
+	if (addr >= addr_reg_addr && addr < (addr_reg_addr + reg_range))
+		addr_reg = data;
+	else if (addr >= data_reg_addr && addr < (data_reg_addr + reg_range))
+		shadow_internal_regs[addr_reg] = data;
+}
+
+
